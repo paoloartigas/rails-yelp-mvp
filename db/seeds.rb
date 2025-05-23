b9061c804322313e00763f94ace2ddeb0b5d50cb
Paolo Lenin Artigas Chacin <paoloartigas21@gmail.com>
2025-05-22 21:39:49 -0400

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+restaurants = [
+  { name: "Epicure", address: "75008 Paris", phone_number: "01 43 54 23 31", category: "french" },
+  { name: "Sushi Zen", address: "Tokyo", phone_number: "81 3-1234-5678", category: "japanese" },
+  { name: "Pasta Fiesta", address: "Rome", phone_number: "06 1234 5678", category: "italian" },
+  { name: "Wok Express", address: "Beijing", phone_number: "010 1234 5678", category: "chinese" },
+  { name: "Frites & Moules", address: "Brussels", phone_number: "02 123 45 67", category: "belgian" }
+]
+
+restaurants.each do |attrs|
+  Restaurant.create!(attrs)
+end
+
+puts "Finished creating restaurants!"
